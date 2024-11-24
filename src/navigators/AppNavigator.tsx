6ca516948422ec092537f39723c277992a1c0ad0
ErenElagz <erenelagoz@gmail.com>
2024-11-24 18:58:05 +0300

    Enhance AppNavigator with tab bar icons and adjust styles; update RootNavigator to simulate logged-in state

--- src/navigators/AppNavigator.tsx
+++ src/navigators/AppNavigator.tsx
@@ -8,30 +8,48 @@ import CommunityScreen from '../screens/CommunityScreen';
 import AssistantScreen from '../screens/AssistantScreen';
 // Styles
 import Colors from '../styles/colors';
+// Components
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 export default function AppNavigator() {
   return (
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({route}) => ({
-          tabBarActiveTintColor: Colors.primary,
-          tabBarInactiveTintColor: Colors.light,
-          tabBarStyle: {
-            height: 60,
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderColor: Colors.borderColor,
-            backgroundColor: Colors.backgroundColor,
-          },
-          headerShown: false,
-        })}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Community" component={CommunityScreen} />
-        <Tab.Screen name="RentCycle" component={RentCycleScreen} />
-        <Tab.Screen name="Assistant" component={AssistantScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({route}) => ({
+        tabBarActiveTintColor: Colors.primary,
+        tabBarInactiveTintColor: Colors.light,
+        tabBarStyle: {
+          height: 66,
+          paddingTop: 8,
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderColor: Colors.borderColor,
+          backgroundColor: Colors.backgroundColor,
+        },
+        headerShown: false,
+        tabBarIcon(props) {
+          let iconName = '';
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Community') {
+            iconName = 'account-group';
+          } else if (route.name === 'RentCycle') {
+            iconName = 'bike';
+          } else if (route.name === 'Assistant') {
+            iconName = 'assistant';
+          } else if (route.name === 'Profile') {
+            iconName = 'account';
+          }
+          return <Icon name={iconName} size={props.size} color={props.color} />;
+        },
+      })}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Community" component={CommunityScreen} />
+      <Tab.Screen name="RentCycle" component={RentCycleScreen} />
+      <Tab.Screen name="Assistant" component={AssistantScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   );
 }
