bf138bec2b81bfe947c5081555425f7b02d3ecbd
ErenElagz <erenelagoz@gmail.com>
2025-01-05 15:38:09 +0300

    Refactor screens to include consistent styling and layout for improved UI

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 export default function HomeScreen() {
   return (
-    <View>
-      <Text >Home Screen</Text>
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function ProfileScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Profile Screen</Text>
     </View>
   );
