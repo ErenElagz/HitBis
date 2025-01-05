fd31149923502522a15942f7df47e157dafbc27f
ErenElagz <erenelagoz@gmail.com>
2025-01-05 14:51:57 +0300

    Update tab bar colors, refine Home screen text, and add Colors and Fonts styles

---
+++ src/styles/Fonts.ts
@@ -0,0 +1,13 @@
+const Fonts = {
+  main: 'CalSans',
+  interExtraLight: 'Inter_ExtraLight',
+  interLight: 'Inter_Light',
+  interThin: 'Inter_Thin',
+  interRegular: 'Inter_Regular',
+  interMedium: 'Inter_Medium',
+  interSemiBold: 'Inter_SemiBold',
+  interBold: 'Inter_Bold',
+  interExtraBold: 'Inter_ExtraBold',
+};
+
+export default Fonts;
