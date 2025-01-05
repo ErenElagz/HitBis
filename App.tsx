dde320032b2e601cf3e2375a42e920be81409c6a
ErenElagz <erenelagoz@gmail.com>
2025-01-05 13:46:23 +0300

    New Version

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {StatusBar} from 'react-native';
-import RootNavigator from './src/navigators/RootNavigator';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <GestureHandlerRootView>
-        <StatusBar barStyle="light-content" backgroundColor={'transparent'} translucent={true} />
-        <RootNavigator />
-      </GestureHandlerRootView>
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor : 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
